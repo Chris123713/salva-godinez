637c3c6880df9bbd739633204c644a8088af8c67
Chris123713 <chrisjochum6@gmail.com>
2026-01-25 21:11:52 -0500

    Initial commit

---
+++ [Sierra Valley RP]/[scripts]/[bcs_housing]/cfx_como_davis/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'Como Sans'
+description 'Walkin MLO Shells'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/unlocked/*.*'
+}
+
+dependency '/assetpacks'
+
+-- To Purchase more quality maps visit https://onlybanns.tebex.io/
+
+-- Any issues or want to learn more about Only Banns RP, join our discord https://discord.gg/vfgKPaEUra
+
+-- Looking to edit, fix, repair or optimize existing maps & other assets, send a message on discord : comosans
+dependency '/assetpacks'
